c81cee260ed8eea0ad6a4cf7669b3da0858e4ac2
tuanld <ledangtuanbk@gmail.com>
2019-05-22 14:59:23 +0700

    update email

--- email.sh
+++ email.sh
@@ -2,16 +2,9 @@ echo "`date` start "
 echo "emails $1"
 echo "title $2"
 echo "content $3"
-api=''
-if [ -z "$api" ]
-then
-      echo "\$api is empty"
-fi
-curl -s --user "api:$api" \
-         https://api.mailgun.net/v3/sandbox8f0fc903cba14b3eac1b4358133fe9ea.mailgun.org/messages \
-         -F from='LDT <ledangtuanbk@gmail.com>' \
-         -F to="$1" \
-         -F subject="$2" \
-         -F text="$3"
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data "{\"email\":{\"title\":\"$2\",\"content\":\"$3\",\"emails\":\"$1\"}}"\
+  http://ssh.isofh.com:9463/email/send
 echo "`date` end"
 
